db7585af833f5f748688431f7d3b8074baa7e85e
Night Cat <107802416+MHNightCat@users.noreply.github.com>
2024-05-30 18:09:54 +0800

    Update and ready to go!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,70 @@
-import Image from "next/image";
+"use client"
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [lastClickTime, setLastClickTime] = useState(0);
+  const [intervalTime, setIntervalTime] = useState(0);
+  const [boxColor, setBoxColor] = useState('bg-gruvbox-gray');
+  const [clickCount, setClickCount] = useState(0);
+  const [underThresholdCount, setUnderThresholdCount] = useState(0);
+  const [threshold, setThreshold] = useState(0.075);
+  const [underThresholdPercentage, setUnderThresholdPercentage] = useState(0);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const handleClick = () => {
+    const now = Date.now();
+    const currentClickCount = clickCount + 1;
+
+    setClickCount(currentClickCount);
+
+    if (lastClickTime !== 0) {
+      const interval = (now - lastClickTime) / 1000;
+      setIntervalTime(interval);
+      if (interval < threshold) {
+        setBoxColor('bg-gruvbox-red');
+        setUnderThresholdCount(underThresholdCount + 1);
+      }
+    }
+    setLastClickTime(now);
+    setUnderThresholdPercentage((underThresholdCount / currentClickCount) * 100);
+  };
+
+  const handleThresholdChange = (e: any) => {
+    const newThreshold = parseFloat(e.target.value);
+    setThreshold(newThreshold);
+  };
+
+  const handleContextMenu = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gruvbox-bg bg-grid">
+      <div
+        className={`w-56 h-56 ${boxColor} flex items-center justify-center border-2 border-gruvbox-light-gray rounded-lg shadow-lg cursor-pointer transition-transform duration-300 hover:scale-110`}
+        onMouseDown={handleClick}
+        onContextMenu={handleContextMenu}
+      >
+        <span className="text-gruvbox-yellow no-select">Click me</span>
+      </div>
+      <div className="mt-6 p-4 bg-gruvbox-dark rounded-lg shadow-lg w-100 no-select">
+        <h2 className="text-gruvbox-yellow text-lg">Interval between two clicks: <span className="text-gruvbox-blue">{intervalTime.toFixed(5)}</span> (s)</h2>
+        <h2 className="text-gruvbox-yellow text-lg">Total click: <span className="text-gruvbox-blue">{clickCount}</span></h2>
+        <h2 className="text-gruvbox-yellow text-lg">Double click count: <span className="text-gruvbox-blue">{underThresholdCount}</span></h2>
+        <h2 className="text-gruvbox-yellow text-lg">Double click percentage: <span className="text-gruvbox-blue">{underThresholdPercentage.toFixed(2)}%</span></h2>
+        <label htmlFor="threshold" className="text-gruvbox-yellow text-lg">Quick click threshold(second): <span className="text-gruvbox-blue">{threshold.toFixed(3)}</span></label>
+        <input
+          type="range"
+          id="threshold"
+          name="threshold"
+          min="0.001"
+          max="1"
+          step="0.001"
+          value={threshold}
+          onChange={handleThresholdChange}
+          className="w-full mt-2"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <a href="https://github.com/yorukot/fixyourmouse" target="_blank" rel="noopener noreferrer" className="text-gruvbox-yellow text-lg mt-4 no-select">Built with ❤️ by Yorukot | Github page</a>
+    </div>
   );
 }
